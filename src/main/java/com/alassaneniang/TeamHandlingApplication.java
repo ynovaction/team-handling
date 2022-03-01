329b622f4b518951896e997624c1ea6941dfcaa3
Alassane NIANG <adniang75@gmail.com>
2022-03-01 13:34:34 +0100

    Configure MainClass

---
+++ src/main/java/com/alassaneniang/TeamHandlingApplication.java
@@ -0,0 +1,11 @@
+package com.alassaneniang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamHandlingApplication {
+    public static void main ( String[] args ) {
+        SpringApplication.run( TeamHandlingApplication.class, args );
+    }
+}
