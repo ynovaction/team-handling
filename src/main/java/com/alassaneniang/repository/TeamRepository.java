3e598a2c6d48d4ddcabf1ea44434285859b73f71
Alassane NIANG <adniang75@gmail.com>
2022-03-02 12:54:48 +0100

    [issue 4] Repository Init
    Repository initial implementation
    
    Closes #4

---
+++ src/main/java/com/alassaneniang/repository/TeamRepository.java
@@ -0,0 +1,8 @@
+package com.alassaneniang.repository;
+
+import com.alassaneniang.entity.TeamEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<TeamEntity, Long> {
+
+}
